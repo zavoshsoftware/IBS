9b73def407949823fc1c6d4271ea9c732b53d635
mohammad reza heidari <mrheidarimk@gmail.com>
2022-06-15 16:28:21 +0430

    ...

--- Site/IBS/IBS/Controllers/HomeController.cs
+++ Site/IBS/IBS/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using ViewModels;
 
 namespace IBS.Controllers
 {
     public class HomeController : Controller
     {
+        private DatabaseContext db = new DatabaseContext();
+
         // GET: Home
         public ActionResult Index()
         {
@@ -21,9 +25,75 @@ namespace IBS.Controllers
         {
             return View();
         }
+
         public ActionResult Dashboard()
         {
-            return View();
+            var i = 1;
+            var UserId = Guid.Parse(User.Identity.Name);
+            var userQuestionnairesIds = db.UserQuestionnaires.Where(w => w.IsDeleted == false && w.UserId == UserId).OrderBy(o => o.CreationDate).ToList();
+            var answers = db.UserQuestionnaireDetails.Where(w => w.IsDeleted == false);
+            List<UserQuestionDetailViewModel> userQuestionDetailViewModelList = new List<UserQuestionDetailViewModel>();
+            List<string> questionIds = new List<string>(new string[] { "3b96726a-8a0d-487c-b992-1a31fa6fd0d7", "802374e7-0c5d-4dd8-b88e-e8c9139bb298", "4ec1ece0-44db-4574-8895-af0086f1ba1a" });
+
+            foreach (var questionId in questionIds)
+            {
+                var Id =Guid.Parse(questionId);
+                foreach (var item in userQuestionnairesIds)
+                {
+                    var answer = answers.FirstOrDefault(w => w.UserQuestionnaireId == item.Id && w.QuestionId == Id);
+                    userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
+                    {
+                        UserQuestionnaireId = answer.UserQuestionnaireId,
+                        QuestionId = questionId,
+                        WeekName = $"week{i++}",
+                        Answer = answer.Answer,
+                        IsDeleted = answer.IsDeleted,
+                        IsActive = answer.IsActive,
+                        CreationDate = answer.CreationDate
+                    });
+                }
+            }
+            //ViewBag.pain = string.Join(",", userQuestionDetailViewModelList.Where(s => s.QuestionId == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Select(m => m.Answer));
+            //ViewBag.Bloating = string.Join(",", userQuestionDetailViewModelList.Where(s => s.QuestionId == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Select(m => m.Answer));
+            //ViewBag.WellBeing = string.Join(",", userQuestionDetailViewModelList.Where(s => s.QuestionId == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Select(m => m.Answer));
+
+            ViewBag.Json = Newtonsoft.Json.JsonConvert.SerializeObject(userQuestionDetailViewModelList);
+            return View(userQuestionDetailViewModelList);
         }
+
+        [HttpGet]
+        public string GetChart()
+        {
+            var i = 1;
+            var UserId = Guid.Parse(User.Identity.Name);
+            var userQuestionnairesIds = db.UserQuestionnaires.Where(w => w.IsDeleted == false && w.UserId == UserId).OrderBy(o => o.CreationDate).ToList();
+            var answers = db.UserQuestionnaireDetails.Where(w => w.IsDeleted == false);
+            List<UserQuestionDetailViewModel> userQuestionDetailViewModelList = new List<UserQuestionDetailViewModel>();
+            List<string> questionIds = new List<string>(new string[] { "3b96726a-8a0d-487c-b992-1a31fa6fd0d7", "802374e7-0c5d-4dd8-b88e-e8c9139bb298", "4ec1ece0-44db-4574-8895-af0086f1ba1a" });
+
+            foreach (var questionId in questionIds)
+            {
+                var Id = Guid.Parse(questionId);
+                foreach (var item in userQuestionnairesIds)
+                {
+                    var answer = answers.FirstOrDefault(w => w.UserQuestionnaireId == item.Id && w.QuestionId == Id);
+                    userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
+                    {
+                        UserQuestionnaireId = answer.UserQuestionnaireId,
+                        QuestionId = questionId,
+                        WeekName = $"week{i++}",
+                        Answer = answer.Answer,
+                        IsDeleted = answer.IsDeleted,
+                        IsActive = answer.IsActive,
+                        CreationDate = answer.CreationDate
+                    });
+                }
+            }
+
+            var res = Newtonsoft.Json.JsonConvert.SerializeObject(userQuestionDetailViewModelList);
+            return res;
+
+        }
+
     }
 }
---
+++ Site/IBS/IBS/ViewModels/UserQuestionDetailViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModels
+{
+    public class UserQuestionDetailViewModel
+    {
+        public Guid UserQuestionnaireId { get; set; }
+
+        public string QuestionId { get; set; }/* = { "3b96726a-8a0d-487c-b992-1a31fa6fd0d7", "802374e7-0c5d-4dd8-b88e-e8c9139bb298", "4ec1ece0-44db-4574-8895-af0086f1ba1a" };*/
+
+        public string Answer { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string WeekName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
