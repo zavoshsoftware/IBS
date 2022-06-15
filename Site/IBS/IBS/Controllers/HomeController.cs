534efab0e9d96b99ff21cf2562e8bf591f8d7d5d
efdefd213 <e.farahani@ymail.com>
2022-06-15 22:24:14 +0430

    Get chart dynamic

--- Site/IBS/IBS/Controllers/HomeController.cs
+++ Site/IBS/IBS/Controllers/HomeController.cs
@@ -28,72 +28,98 @@ namespace IBS.Controllers
 
         public ActionResult Dashboard()
         {
-            var i = 1;
-            var UserId = Guid.Parse(User.Identity.Name);
-            var userQuestionnairesIds = db.UserQuestionnaires.Where(w => w.IsDeleted == false && w.UserId == UserId).OrderBy(o => o.CreationDate).ToList();
-            var answers = db.UserQuestionnaireDetails.Where(w => w.IsDeleted == false);
-            List<UserQuestionDetailViewModel> userQuestionDetailViewModelList = new List<UserQuestionDetailViewModel>();
-            List<string> questionIds = new List<string>(new string[] { "3b96726a-8a0d-487c-b992-1a31fa6fd0d7", "802374e7-0c5d-4dd8-b88e-e8c9139bb298", "4ec1ece0-44db-4574-8895-af0086f1ba1a" });
-
-            foreach (var questionId in questionIds)
-            {
-                var Id =Guid.Parse(questionId);
-                foreach (var item in userQuestionnairesIds)
-                {
-                    var answer = answers.FirstOrDefault(w => w.UserQuestionnaireId == item.Id && w.QuestionId == Id);
-                    userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
-                    {
-                        UserQuestionnaireId = answer.UserQuestionnaireId,
-                        QuestionId = questionId,
-                        WeekName = $"week{i++}",
-                        Answer = answer.Answer,
-                        IsDeleted = answer.IsDeleted,
-                        IsActive = answer.IsActive,
-                        CreationDate = answer.CreationDate
-                    });
-                }
-            }
-            //ViewBag.pain = string.Join(",", userQuestionDetailViewModelList.Where(s => s.QuestionId == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Select(m => m.Answer));
-            //ViewBag.Bloating = string.Join(",", userQuestionDetailViewModelList.Where(s => s.QuestionId == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Select(m => m.Answer));
-            //ViewBag.WellBeing = string.Join(",", userQuestionDetailViewModelList.Where(s => s.QuestionId == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Select(m => m.Answer));
-
-            ViewBag.Json = Newtonsoft.Json.JsonConvert.SerializeObject(userQuestionDetailViewModelList);
-            return View(userQuestionDetailViewModelList);
+            var scoreCount = GetChart("Pain", "3b96726a-8a0d-487c-b992-1a31fa6fd0d7").Length;
+            ViewBag.Pain = GetChart("Pain", "3b96726a-8a0d-487c-b992-1a31fa6fd0d7");
+            ViewBag.Bloating = GetChart("Bloating", "802374e7-0c5d-4dd8-b88e-e8c9139bb298");
+            ViewBag.WellBeing = GetChart("WellBeing", "4ec1ece0-44db-4574-8895-af0086f1ba1a");
+            ViewBag.Score = GetWeek(scoreCount);
+            return View();
         }
 
-        [HttpGet]
-        public string GetChart()
+        public object[] GetChart(string type, string id)
         {
-            var i = 1;
+            object[] chartItem = new object[]{};
             var UserId = Guid.Parse(User.Identity.Name);
-            var userQuestionnairesIds = db.UserQuestionnaires.Where(w => w.IsDeleted == false && w.UserId == UserId).OrderBy(o => o.CreationDate).ToList();
+            var userQuestionnaires = db.UserQuestionnaires.Where(w => w.IsDeleted == false && w.UserId == UserId).OrderBy(o => o.CreationDate).ToList();
             var answers = db.UserQuestionnaireDetails.Where(w => w.IsDeleted == false);
             List<UserQuestionDetailViewModel> userQuestionDetailViewModelList = new List<UserQuestionDetailViewModel>();
-            List<string> questionIds = new List<string>(new string[] { "3b96726a-8a0d-487c-b992-1a31fa6fd0d7", "802374e7-0c5d-4dd8-b88e-e8c9139bb298", "4ec1ece0-44db-4574-8895-af0086f1ba1a" });
-
-            foreach (var questionId in questionIds)
+            foreach (var userQuestionId in userQuestionnaires)
             {
-                var Id = Guid.Parse(questionId);
-                foreach (var item in userQuestionnairesIds)
+                var Id = userQuestionId.Id;
+                var questionId = Guid.Parse(id);
+
+                var answer = answers.FirstOrDefault(w => w.UserQuestionnaireId == Id && w.QuestionId == questionId);
+                if (id == "3b96726a-8a0d-487c-b992-1a31fa6fd0d7")
                 {
-                    var answer = answers.FirstOrDefault(w => w.UserQuestionnaireId == item.Id && w.QuestionId == Id);
-                    userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
+                    if (answer != null)
                     {
-                        UserQuestionnaireId = answer.UserQuestionnaireId,
-                        QuestionId = questionId,
-                        WeekName = $"week{i++}",
-                        Answer = answer.Answer,
-                        IsDeleted = answer.IsDeleted,
-                        IsActive = answer.IsActive,
-                        CreationDate = answer.CreationDate
-                    });
+                        userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
+                        {
+                            PainAnswer = answer.Answer,
+                        });
+                    }
+                    chartItem = new object[userQuestionDetailViewModelList.Count + 1];
+                    chartItem[0] = type;
+                    for (int j = 0; j < userQuestionDetailViewModelList.Count; j++)
+                    {
+                        chartItem[j + 1] = Int32.Parse(userQuestionDetailViewModelList[j].PainAnswer);
+                    }
+
                 }
+                else if (id == "802374e7-0c5d-4dd8-b88e-e8c9139bb298")
+                {
+                    if (answer != null)
+                    {
+                        userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
+                        {
+                            BloatingAnswer = answer.Answer,
+                        });
+                    }
+                    chartItem = new object[userQuestionDetailViewModelList.Count + 1];
+                    chartItem[0] = type;
+                    for (int j = 0; j < userQuestionDetailViewModelList.Count; j++)
+                    {
+                        chartItem[j + 1] = Int32.Parse(userQuestionDetailViewModelList[j].BloatingAnswer);
+                    }
+
+                }
+                else if(id == "4ec1ece0-44db-4574-8895-af0086f1ba1a")
+                {
+                    if (answer != null)
+                    {
+                        userQuestionDetailViewModelList.Add(new UserQuestionDetailViewModel()
+                        {
+                            WellBeingAnswer = answer.Answer,
+                        });
+                    }
+                    chartItem = new object[userQuestionDetailViewModelList.Count + 1];
+                    chartItem[0] = type;
+                    for (int j = 0; j < userQuestionDetailViewModelList.Count; j++)
+                    {
+                        chartItem[j + 1] = Int32.Parse(userQuestionDetailViewModelList[j].WellBeingAnswer);
+                    }
+
+                }
+
             }
 
-            var res = Newtonsoft.Json.JsonConvert.SerializeObject(userQuestionDetailViewModelList);
-            return res;
+            
+            return chartItem;
 
         }
 
-    }
+        public object[] GetWeek(int weekCount)
+        {
+            object[] chartWeek = new object[] { };
+            chartWeek = new object[weekCount];
+            chartWeek[0] = "Score";
+            for (int j = 1; j < weekCount; j++)
+            {
+                chartWeek[j] = "Week"+ j.ToString();
+            }
+            return chartWeek;
+        }
+        
+
+        }
 }
